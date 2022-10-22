488d87d6ab1726790552b780f4110eb7ae991144
MD ASHRAF UDDIN <43129513+techtobit@users.noreply.github.com>
2022-10-23 01:58:10 +0600

    base-color

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
           secondary: "#f6d860",
           accent: "#37cdbe",
           neutral: "#3d4451",
-          "base-100": "#ffffff",
+          "base-100": "#fffff1",
         },
       },
       "dark",
